cd49cf29e22eacceb2fd1c9c2f8d272a69ef1138
Rubens-Zan <rubens.zandom@gmail.com>
2021-11-24 19:32:47 -0300

    Finalizando readme

--- entrega/GRR20206147/src/arvoreSecundaria.h
+++
@@ -1,8 +0,0 @@
-#include "estruturas.h"
-tArvoreB *montaArvoreSecundaria(const char *str);
-tArvoreB *criaNo(int chave);
-tArvoreB *inclui(tArvoreB *no, int c);
-int numeroEntreParenteses(const char *str, int *indice); 
-tArvoreB *montaarvore(const char *str);
-void imprimeNosEmOrdem (tArvoreB *no);  
-void imprimeNosPreOrdem (tArvoreB *no); 
